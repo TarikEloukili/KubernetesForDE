32ae26da42fd7ba71ed3350d18cd853ae00e6895
TarikEloukili <tarikeloukili03@gmail.com>
2025-05-04 00:26:06 +0100

    initial commit

---
+++ dags/hello.py
@@ -0,0 +1,30 @@
+from airflow import DAG
+from airflow.operators.bash import BashOperator
+from deatetime import datetime, timedelta
+
+
+default_args = {
+    'owner': 'tarik',
+    'start_date': datetime(2024, 1, 25),
+    'catchup': False,
+}
+
+dag = DAG(
+    'hello_world',
+    default_args = default_args,
+    schedule= timedelta(days=1)
+)
+
+t1= BashOperator(
+    task_id = 'hello_world',
+    bash_command ='echo "Hello world"',
+    dag = dag
+)
+
+t2 = BashOperator(
+    task_id = 'hello_tarik',
+    bash_command ='echo "Hello Tarik"',
+    dag = dag
+)
+
+t1 >> t2
